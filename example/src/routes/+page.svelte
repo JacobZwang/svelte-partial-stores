315f53577c8cf7cccfd6dce61952955c8a0422e2
JacobZwang <59858341+JacobZwang@users.noreply.github.com>
2024-02-26 11:03:26 -0800

    start work on example
    this is from a long time ago. i'm pushing because I'm resetting my computer

---
+++ example/src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import { triggered } from '../../../src/index';
+    import { type Writable, writable } from 'svelte/store';
+
+    const store: Writable<
+        | {
+              name: {
+                  first: string;
+                  last: string;
+              };
+              id: string;
+          }
+        | undefined
+    > = writable({
+        name: {
+            first: 'Jacob',
+            last: 'Zwang'
+        },
+        id: 'id'
+    });
+
+    interface Test {
+        name: {
+            last: boolean;
+        };
+        id: boolean;
+    }
+
+    const test = triggered();
+
+    $: console.log($test);
+</script>
+
+<!-- 
+<label>
+    first name
+    <input type="text" bind:value={$store.name.first} />
+</label>
+
+<label>
+    last name
+    <input type="text" bind:value={$store.name.last} />
+</label>
+
+<label>
+    id
+    <input type="text" bind:value={$store.id} />
+</label> -->
